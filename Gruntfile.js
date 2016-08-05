0712680ea5111095e7db9cc3282a62571ce2d328
John Freeman <john.freeman.opensource@gmail.com>
2016-08-05 22:13:14 +0100

    Added config to display presention
    Used grunt connect to run web server.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,14 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     // Task configuration.
+    connect: {
+      server: {
+        options: {
+          port: 8080,
+          base: ['.', 'node_modules/reveal.js' ]
+        }
+      }
+    },
     jshint: {
       options: {
         curly: true,
@@ -28,15 +36,27 @@ module.exports = function(grunt) {
         files: '<%= jshint.gruntfile.src %>',
         tasks: ['jshint:gruntfile']
       },
+      html: {
+        files: [ '*.html']
+      },
+      markdown: {
+        files: [ '*.md' ]
+      },
+      options: {
+        livereload: true
       }
     }
   });
 
   // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  // Default task.
-  grunt.registerTask('default', ['jshint']);
+  // Serve presentation locally.
+  grunt.registerTask('serve', ['connect', 'watch']);
+
+  // Run tests.
+  grunt.registerTask('test', ['jshint']);
 
 };
